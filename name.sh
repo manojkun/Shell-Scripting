8ef4071d9401bd4a825081d82695aaa5bdd7e740
manojkun <r.nanimanoj@gmail.com>
2017-12-25 23:22:03 +0530

    some scripts

---
+++ name.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#Script to Output the given name
+
+read name
+echo "Welcome $name"
+#end
---
+++ oddnumprint.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#Bash Script to print Odd Numbers from 1 to 99
+
+for ((i=1;i<99;i=i+2))
+do
+	echo $i
+done
